0c6c6c34781bb634b60d40d13fb24cd39a40d9b8
Edgar147 <karapetyane1996@gmail.com>
2021-12-11 22:28:47 +0100

    With random words from english 1000 word dictionary.

--- app/src/main/java/com/example/typingspeedkotlin/MainActivity.kt
+++ app/src/main/java/com/example/typingspeedkotlin/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.typingspeedkotlin
 
-
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.typingspeedkotlin.databinding.ActivityMainBinding
-
-
-import android.content.res.Resources
-
+import java.io.BufferedInputStream
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -22,63 +18,55 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        var dictos = R.raw.dictos
+        var ag = BufferedInputStream(getResources().openRawResource(dictos))
+        var inputAsString =
+            ag.bufferedReader().use { it.readLines() } // from input file to List of String
 
+        var textIntegral: String = ""
 
-        var textIntegral: String= "are you sure you want to type hello some sun house give that is another wine for football "
+        for (i in 1..200) {
+            var myRandom = (0..998).random()
+            textIntegral = textIntegral + inputAsString.elementAt(myRandom) + " "
+        }
+
         var strs = textIntegral.split(" ").toMutableList()
-        binding.tvText.text=strs[0]
-        val x=binding.tvText.text
-        val nextButton=binding.bNext
-        var score=0
+        binding.tvText.text = strs[0]
+        val x = binding.tvText.text
+        val nextButton = binding.bNext
+        var score = 0
 
-        //val fileContent = MySpec::class.java.getResource("/html/file.html").readText()
 
         binding.bNext.setOnClickListener {
-
-            if(binding.tvText.text.toString().equals(binding.edTyping.text.toString())){
+            if (binding.tvText.text.toString().equals(binding.edTyping.text.toString())) {
                 strs.removeAt(0)
-                binding.tvText.text=strs[0]
+                binding.tvText.text = strs[0]
                 binding.edTyping.setText("")
                 binding.edTyping.setBackgroundColor(Color.WHITE)
                 score++
-
-            }
-            else{
+            } else {
                 binding.edTyping.setBackgroundColor(Color.RED)
-
             }
-            var y =binding.edTyping.text
-            Log.i("first","$x")
-            Log.i("second","$y")
+            var y = binding.edTyping.text
 
         }
 
+        val timer =
+            object : CountDownTimer(60000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    binding.tvTimer.text = (millisUntilFinished / 1000).toString()
+                }
 
-
-
-        val timer = object: CountDownTimer(10000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                binding.tvTimer.text=(millisUntilFinished / 1000).toString()
+                override fun onFinish() {
+                    binding.tvFinal.visibility = View.GONE
+                    binding.edTyping.visibility = View.GONE
+                    binding.tvText.visibility = View.GONE
+                    binding.tvTimer.visibility = View.GONE
+                    binding.tvScore.visibility = View.VISIBLE
+                    binding.bNext.visibility = View.GONE
+                    binding.tvScore.text = "Your score: " + score
+                }
             }
-
-
-            override fun onFinish() {
-                binding.tvFinal.visibility=View.GONE
-                binding.edTyping.visibility=View.GONE
-                binding.tvText.visibility=View.GONE
-                binding.tvTimer.visibility=View.GONE
-                binding.tvScore.visibility=View.VISIBLE
-                binding.bNext.visibility=View.GONE
-                binding.tvScore.text="Your score: "+score
-
-
-
-            }
-        }
         timer.start()
     }
-
-
-
-
 }
